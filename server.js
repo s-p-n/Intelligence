0bdf616984381858ea17d88afaef1ad79ff77083
s-p-n <coderspence@gmail.com>
2015-09-16 16:14:08 -0600

    Added some bootstrap and basic site

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-  res.send('CAN HAZ INTELLIGENCE???');
+var user = {
+	logged_in: false;
+};
+
+app.get('/', function (req, res) {
+	if (user.logged_in) {
+		console.log("Strange..");
+	} else {
+		res.sendFile('./public/login.html')
+	}
+});
+
+app.get('/assets/:type/:file', function (req, res) {
+	var type = req.params.type;
+	var file = req.params.file;
+	res.sendFile('./public/' + type + "/" + file);
 });
 
 app.listen(80);
